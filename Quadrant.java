c6ad2de5d0f79f2f275abdf55567e9a388b1a56c
j-inhee <ihcho8@naver.com>
2021-03-20 18:04:51 +0900

    알람 시간

--- Quadrant.java
+++ Quadrant.java
@@ -9,14 +9,27 @@ public class Quadrant {
         x = sc.nextInt();
         y = sc.nextInt();
 
-        if(x>0 && y>0)
+        if(x>0){
+            if(y>0)
+                System.out.println("1");
+            else
+                System.out.println("4");
+        }
+        else if(x<0){
+            if(y>0)
+                System.out.println("2");
+            else
+                System.out.println("4");
+        }
+
+        /*if(x>0 && y>0)
             System.out.println("1");
         else if(x<0 && y>0)
             System.out.println("2");
         else if(x<0 && y<0)
             System.out.println("3");
         else
-            System.out.println("4");
+            System.out.println("4");*/
     }
 
 }
